88b50179fad3d97e89247723a6eea4bba54a1648
郭东宇 <yudong.guo@ruhlamatst.onaliyun.com>
2024-02-18 15:51:06 +0800

    更换json-server的版本

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,39 +1,59 @@
-import { createAlova } from 'alova'
+import { Method, createAlova } from 'alova'
 import GlobalFetch from 'alova/GlobalFetch'
 import VueHook from 'alova/vue'
 
 import { useGlobalStore } from '@/stores/global'
 
-const beforeRequest = (method: any) => {
+const beforeRequest = (method: Method) => {
   // 假设我们需要添加token到请求头
   // method.config.headers.common.Authorization = ''
   const globalStore = useGlobalStore()
   globalStore.isLoading = true
 }
-const responded = async (response: any) => {
-  const globalStore = useGlobalStore()
-  const json = await response.json()
-  globalStore.isLoading = false
+const onSuccess = async (response: any) => {
+  console.log(response.statusText, 'response.statusText')
 
+  if (response.status >= 400) {
+    throw new Error(response.statusText)
+  }
+  const json = await response.json()
   if (json.code !== 200) {
     // 抛出错误或返回reject状态的Promise实例时，此请求将抛出错误
     throw new Error(json.message)
   }
+
+  // 解析的响应数据将传给method实例的transformData钩子函数，这些函数将在后续讲解
   return json.data
 }
 
-export const commentsAlova = createAlova({
+// 请求错误时将会进入该拦截器。
+// 第二个参数为当前请求的method实例，你可以用它同步请求前后的配置信息
+const onError = (err: any, method: Method) => {
+  console.log(err, method, 'onError')
+}
+
+const onComplete = (method: Method) => {
+  // 处理请求完成逻辑
+  const globalStore = useGlobalStore()
+  globalStore.isLoading = false
+}
+
+const BaseAlovaOption = {
   baseURL: '/api',
   statesHook: VueHook,
   requestAdapter: GlobalFetch(),
-  beforeRequest: (method) => beforeRequest(method),
-  responded: (response) => responded(response)
+  beforeRequest: (method: Method) => beforeRequest(method),
+  responded: {
+    onSuccess: (response) => onSuccess(response),
+    onError: (err, method) => onError(err, method),
+    onComplete: (method) => onComplete(method)
+  }
+}
+
+export const commentsAlova = createAlova({
+  ...BaseAlovaOption
 })
 
 export const userAlova = createAlova({
-  baseURL: '/api',
-  statesHook: VueHook,
-  requestAdapter: GlobalFetch(),
-  beforeRequest: (method) => beforeRequest(method),
-  responded: (response) => responded(response)
+  ...BaseAlovaOption
 })
